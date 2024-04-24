519b8a3b8a8d5edbe00e9d9a8f5787cef262f703
Prateek Deshmukh <prateek@Prateeks-MacBook-Air.local>
2024-04-25 02:00:23 +0530

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,111 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    * {
+        background-color: #f5f5f5;
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        padding-top: 1vh;
+    }
+    body {
+        height: 100vh;
+        padding: 0;
+    }
+    
+    .container {
+        max-width: 800px;
+        padding: 0 20px;
+    }
+    
+    hr {
+        border: 0;
+        border-top: 2px solid;
+        margin-top: 5px;
+    }
+    .jersey-15-regular {
+        font-family: "Jersey 15", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 50px;
+        text-align: left;
+    }
+    .content {
+        text-align: left;
+    }
+    p {
+            color: #000;
+            font: 400 19px/1.5 'Anek Devanagari', sans-serif;
+        }
+    .codeday {
+        color: #FF686B;
+        cursor: pointer;
+        text-decoration: none;
+
+    }
+    .codeday:hover {
+        text-decoration: underline;
+        background-color: #FF686B;
+        color: white;
+    }
+    .sicp {
+        color: cadetblue;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    .sicp:hover {
+        text-decoration: underline;
+        background-color: cadetblue;
+        color: white;
+    }
+    .email {
+        cursor: pointer;
+        text-decoration: none;
+        color: #000;
+    }
+    .email:hover {
+        color: #fff;
+        background-color: #000;
+        text-decoration: underline;
+    }
+    
+</style>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jersey+15&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@100..800&family=Hind+Madurai:wght@300;400;500;600;700&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Prateek Deshmukh</title>
+</head>
+
+<body>
+    <center>
+    <div class="container">
+        
+        <header>
+            <h1 class="jersey-15-regular">Prateek Deshmukh</h1>
+            <hr>
+        </header>
+        
+        <div class="content">
+            <p>Hello there. I'm a full-time engineering sophomore from Bengaluru.</p>
+            
+            <br>
+
+            <p>I love tech, people, and their intersection. I also love explaining complex ideas (ironic, given my propensity to overengineer things) and working on logistically heavy ideas with high impact. <br> I currently do a little bit of everything at [<a class="codeday" link="https://codeday.org">CodeDay</a>]. I meet new people, organize fun events, conduct workshops, ideate and implement new programs, and occasionally don a costume.</p>
+        
+            
+            
+            <br>
+
+            <p>Apart from this I enjoy a good ol' debate, cooking pasta, playing badminton, and watching F1. The driving question all my life has been "What gives?" and I've gone to.. interesting ends in my pursuit to answer it.</p>
+
+            <p>
+                I'm currently trying to read [<a class='sicp' href="https://sarabander.github.io/sicp/html/index.xhtml">SICP</a>] and program in LISP, while also trying to revive my hopes at an academic comeback.
+            </p>
+            <br> <br>
+            <p>Hit me up if you wanna talk about anything, I love a brain-picking session. So... What gives? <br><b>Email:</b> [<a class="email" href="mailto:prateek.deshmukh19@gmail.com">prateek.deshmukh19@gmail.com</a>]</p>
+            
+        </div>
+    </div>
+</center>
+</body>
