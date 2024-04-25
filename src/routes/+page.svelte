45978ed8c01dd71800b65aaf9e44cc7b8519b4f3
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-04-25 22:45:15 +0530

    fix indents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <style lang="postcss">
- .jersey-15-regular {
-     font-family: "Jersey 15", sans-serif;
-     font-size: 50px;
- }
- 
- p {
-     font: 400 19px/1.6 'Anek Devanagari', sans-serif;
- }
+    .jersey-15-regular {
+        font-family: "Jersey 15", sans-serif;
+        font-size: 50px;
+    }
 
- a {
-     font-family: Arial, sans-serif;
- }
+    p {
+        font: 400 19px/1.6 'Anek Devanagari', sans-serif;
+    }
+
+    a {
+        font-family: Arial, sans-serif;
+    }
 </style>
 
 <svelte:head>
@@ -20,18 +20,18 @@
 <div class="flex flex-col items-center min-h-screen p-4">
     <h1 class="max-w-screen-md jersey-15-regular text-left border-b-2 border-neutral-500 mb-5 w-full">Prateek Deshmukh</h1>
     <div class="flex flex-col gap-5 max-w-screen-md text-xl">
-	<p class="inline-block align-top">Hello there. I'm a full-time engineering sophomore from Bengaluru.</p>
+        <p class="inline-block align-top">Hello there. I'm a full-time engineering sophomore from Bengaluru.</p>
 
-	<p class="align-bottom">I love tech, people, and their intersection. I also love explaining complex ideas (ironic, given my propensity to overengineer things) and working on logistically heavy ideas with high impact. <br> I currently do a little bit of everything at [<a class="text-codeday-red hover:text-white hover:bg-codeday-red transition" href="https://codeday.org">CodeDay</a>]. I meet new people, organize fun events, conduct workshops, ideate and implement new programs, and occasionally don a costume.</p>
+        <p class="align-bottom">I love tech, people, and their intersection. I also love explaining complex ideas (ironic, given my propensity to overengineer things) and working on logistically heavy ideas with high impact. <br> I currently do a little bit of everything at [<a class="text-codeday-red hover:text-white hover:bg-codeday-red transition" href="https://codeday.org">CodeDay</a>]. I meet new people, organize fun events, conduct workshops, ideate and implement new programs, and occasionally don a costume.</p>
 
-	<p>Apart from this I enjoy a good ol' debate, cooking pasta, playing badminton, and watching F1. The driving question all my life has been "What gives?" and I've gone to.. interesting ends in my pursuit to answer it.</p>
+        <p>Apart from this I enjoy a good ol' debate, cooking pasta, playing badminton, and watching F1. The driving question all my life has been "What gives?" and I've gone to.. interesting ends in my pursuit to answer it.</p>
 
-	<p>I'm currently trying to read [<a class="text-cadetblue hover:text-white hover:bg-cadetblue transition" href="https://sarabander.github.io/sicp/html/index.xhtml">SICP</a>] and program in LISP, while also trying to revive my hopes at an academic comeback.</p>
-	
-	<p class="mt-5"> Hit me up if you wanna talk about anything, I love a brain-picking session. So... What gives?</p>
-	<p class="mt-[-1em]">
-	    <span class="font-bold">Email:</span>
-	    [<a class="text-black hover:text-white hover:bg-black transition" href="mailto:hello@prateekdeshmukh.com">hello@prateekdeshmukh.com</a>]
-	</p>
+        <p>I'm currently trying to read [<a class="text-cadetblue hover:text-white hover:bg-cadetblue transition" href="https://sarabander.github.io/sicp/html/index.xhtml">SICP</a>] and program in LISP, while also trying to revive my hopes at an academic comeback.</p>
+
+        <p class="mt-5"> Hit me up if you wanna talk about anything, I love a brain-picking session. So... What gives?</p>
+        <p class="mt-[-1em]">
+	        <span class="font-bold">Email:</span>
+	        [<a class="text-black hover:text-white hover:bg-black transition" href="mailto:hello@prateekdeshmukh.com">hello@prateekdeshmukh.com</a>]
+        </p>
     </div>
 </div>
