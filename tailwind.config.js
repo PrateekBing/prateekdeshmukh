21d1dc601f7d7683fa33294a979ebf83d3078fc3
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-04-25 22:42:11 +0530

    Tailwindify

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cadetblue': 'cadetblue',
+        'codeday-red': '#FF686B',
+      },
+    },
   },
   plugins: [],
 }
